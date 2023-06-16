d7c56098af06e8ca2ebe2063beb39bd7dedead8c
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-17 00:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
-    //Write your code here
-  
+  let rows = parseInt(prompt("Input number of rows"));
+  let cols = parseInt(prompt("Input number of columns"));
+  let table = document.getElementById("myTable");
+
+	table.innerHTML = "";
+	
+	for(let i = 0; i < rows; i++)
+	{
+		let row = table.insertRow(i);
+	for(let j = 0; j < cols; j++)
+	{
+		let cell = row.insertCell(j);
+	    cell.textContent = `Row- ${i + 1} Column- ${j + 1}`;
+    }
+  }
 }
